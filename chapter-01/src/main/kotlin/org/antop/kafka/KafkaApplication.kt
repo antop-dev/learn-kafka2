8d383db5de524eaa0e3a56f448e409affef11432
Antop <antop@kakao.com>
2025-04-14 11:17:36 +0900

    메인 클래스명 수정

--- chapter-01/src/main/kotlin/org/antop/kafka/KafkaApplication.kt
+++ chapter-01/src/main/kotlin/org/antop/kafka/KafkaApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Chapter01Application
+class KafkaApplication
 
 fun main(args: Array<String>) {
-    runApplication<Chapter01Application>(*args)
+    runApplication<KafkaApplication>(*args)
 }
